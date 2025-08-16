922fafcba8e47d8eede193f95b45c4256b218ace
rakib hasan <dev.rakibhasan1@gmail.com>
2025-08-16 21:24:33 +0600

    completed filtering and search functionality in VisaServicePage

--- src/Pages/VisaServicePage.jsx
+++ src/Pages/VisaServicePage.jsx
@@ -1,11 +1,98 @@
-import React from 'react';
+
+import { Dropdown, Space } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router';
+import { DownOutlined } from '@ant-design/icons';
 
 const VisaServicePage = () => {
+    const [services, setServices] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState("");
+
+    useEffect(() => {
+        fetch("/services.json")
+            .then(res => res.json())
+            .then(data => setServices(data))
+            .catch(err => console.error("Failed to fetch services:", err));
+    }, [])
+
+    // console.log(services);
+
+    const filteredServices = services.filter(category =>
+        !selectedCategory || category.category === selectedCategory
+    )
+        .map(category => ({
+            ...category,
+            services: category.services.filter(service =>
+                service.title.toLowerCase().includes(searchText.toLowerCase())
+            )
+        }));
+
+
+
+
+
     return (
-        <div>
-            This is Visa Service Page
-            <p>Here you can find information about various visa services.</p>
-            <p>For more details, please contact our support team.</p>
+        <div className="max-w-6xl mx-auto px-4 py-10 fira-sans">
+            <h1 className="text-3xl font-bold text-center mb-6">
+                Visa Services
+            </h1>
+            <div className="flex flex-col lg:flex-row items-center justify-center gap-4 mb-8">
+                
+                <select
+                    value={selectedCategory}
+                    onChange={e => setSelectedCategory(e.target.value)}
+                    className="border border-gray-300 rounded-lg px-4 py-2 bg-white shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                >
+                    <option value="">All Categories</option>
+                    {services.map(cat => (
+                        <option key={cat.category} value={cat.category}>
+                            {cat.category}
+                        </option>
+                    ))}
+                </select>
+
+                {/* Search Input */}
+                <input
+                    type="text"
+                    value={searchText}
+                    onChange={e => setSearchText(e.target.value)}
+                    placeholder="Search service..."
+                    className="border border-gray-300 rounded-full px-5 py-2 w-full lg:w-80 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                />
+            </div>
+
+            {filteredServices.length > 0 ? (
+                filteredServices.map(service => (
+                    <div key={service.category} className="mb-12">
+                        {/* Category Name */}
+                        <h2 className="text-2xl font-semibold text-center mb-6">
+                            {service.category}
+                        </h2>
+
+                        {/* Service Cards */}
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                            {service.services.map(s => (
+                                <Link to={`/service/${s.id}`} key={s.id}>
+                                    <div
+
+                                        className="bg-gray-100 rounded-md p-6 text-center hover:shadow-md transition duration-300"
+                                    >
+
+                                        <img src={s.image} className='w-full h-50 object-cover rounded-md mb-2' alt="service_image" />
+                                        <h3 className="text-xl font-bold mb-2">{s.title}</h3>
+                                        <p className="text-gray-600">Processing Time: {s.processingTime}</p>
+                                        <p className="text-gray-800 font-semibold mt-2">Price: {s.price}</p>
+
+                                    </div>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <h2 className='text-center text-gray-500'>No Category Found</h2>
+            )}
         </div>
     );
 };
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,6 +3,7 @@ import Home from "../Pages/Home";
 import VisaServicePage from "../Pages/VisaServicePage";
 import MyApplicationPage from "../Pages/MyApplicationPage";
 import RootLaout from "../Layouts/RootLaout";
+import ServiceDetails from "../Pages/ServiceDetails";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
       {
         path: "/my-application",
         Component: MyApplicationPage
+      },
+      {
+        path: "/service/:id",
+        Component: ServiceDetails
       }
     ]
   },
